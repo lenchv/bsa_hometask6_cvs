b428fb739fc8f691fc48ef43cf042751a0d9806a
lenchv <lenchvov@rambler.ru>
2016-07-08 21:40:38 +0000

    5: test commit

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once(__DIR__ . "/src/Library/MyClass.php");
 require_once(__DIR__ . "/src/Library/OtherClass.php");
 require_once(__DIR__ . "/src/Library/Decorator.php");
-
+// --
 $oPage = new \Library\Decorator;
 echo $oPage->header("BSA Hometask");
 echo $oPage->body([
