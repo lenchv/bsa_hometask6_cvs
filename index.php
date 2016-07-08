91b76751ae88e5eb51543a43c45f1c2d6dc6096b
lenchv <lenchvov@rambler.ru>
2016-07-08 20:09:08 +0000

    4: add html wrapper to index.php

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
 require_once(__DIR__ . "/src/Library/MyClass.php");
-
-echo \Library\MyClass::dateNow() . PHP_EOL;
+require_once(__DIR__ . "/src/Library/OtherClass.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>BSA Hometask</title>
+</head>
+<body>
+<h1>Current date and time</h1>
+<p>
+<?php echo \Library\MyClass::dateNow() . "<br />"; ?>
+<?php echo \Library\OtherClass::timeNow(); ?>
+</p>
+</body>
+</html>
