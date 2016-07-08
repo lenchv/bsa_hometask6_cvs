50032823214130a170cbebec65e829c4cb7bafd9
lenchv <lenchvov@rambler.ru>
2016-07-08 20:42:56 +0000

    5: add class decorator for make html

---
+++ src/Library/Decorator.php
@@ -0,0 +1,33 @@
+<?php
+namespace Library;
+
+class Decorator {
+  private $header;
+  private $footer;
+  private $body;
+
+  public function header($title) {
+    $this->header = "<!DOCTYPE>" .
+      "<html>" .
+      "<head>" .
+      "<title>{$title}</title>" .
+      "</head>";
+      return $this;
+  }
+
+  public function footer() {
+    $this->footer = "</html>";
+    return $this;
+  }
+
+  public function body($param) {
+    $this->body = "<body>" .
+      "<h1>Current date and time</h1>" .
+      "<p>{$param['date]}<br />{$param['time']}</p>" .
+      "</body>";
+  }
+
+  public function build() {
+    return $this->header . $this->body . $this->footer;
+  }
+}
