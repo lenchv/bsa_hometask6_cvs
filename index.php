32d1a6f58e9ef73438fe288da87624cb276fe313
lenchv <lenchvov@rambler.ru>
2016-07-09 08:50:08 +0000

    7: commit in develop-feature1

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once(__DIR__ . "/src/Library/MyClass.php");
 require_once(__DIR__ . "/src/Library/OtherClass.php");
 require_once(__DIR__ . "/src/Library/Decorator.php");
-
+// commit that exist just in develop-feature1 branch
 $oPage = new \Library\Decorator;
 echo $oPage->header("BSA Hometask");
 echo $oPage->body([
