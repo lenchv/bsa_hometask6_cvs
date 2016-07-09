7f17964d70d1e8b5914ab2282fbe3fe670bdd3e8
lenchv <lenchvov@rambler.ru>
2016-07-09 11:48:03 +0000

    8: bug commit 2

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once(__DIR__ . "/src/Library/MyClass.php");
 require_once(__DIR__ . "/src/Library/OtherClass.php");
 require_once(__DIR__ . "/src/Library/Decorator.php");
 require_once(__DIR__ . "/src/Library/Builder.php");
-// develop1 - bug commit 1
+// develop1 - bug commit 2
 use \Library\{MyClass, OtherClass, Decorator, Builder};
 $header = new Decorator("header", "BSA Hometask");
 $body = new Decorator("body", [
